bc4e171d59470110bb3dd1f7975354e5a3dc8e58
Mark Jordan <mjordan@sfu.ca>
2015-03-27 16:26:39 +0000

    Added files that should have been part of last commit.

---
+++ theme/islandora-context-reaction-inserted-message.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * Template file for the islandora_context_reaction_insert_text
+ * Context plugin.
+ * 
+ * Available variables:
+ *   $message_text string
+ *     The message to display to users.
+ */
+<div class=\"islandora-metadata\"><?php print $message_text; ?></div>
