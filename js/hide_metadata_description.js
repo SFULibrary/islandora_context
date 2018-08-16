692510dd3cb8b750a7004c725cc4f8a62a15627a
Mark Jordan <mjordan@sfu.ca>
2018-08-16 15:16:11 +0000

    Actually add the new files to the issue-50 branch.

---
+++ js/hide_metadata_description.js
@@ -0,0 +1,6 @@
+(function ($) {
+  $(document).ready(function() {
+    $("div.islandora-metadata-sidebar h2").hide();
+    $("div.islandora-metadata-sidebar p[property='description']").hide();
+ });
+})(jQuery);
