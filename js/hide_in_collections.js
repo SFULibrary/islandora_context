b1b1ef90e3a773fff2a8468e6f279323df73ecf3
Natkeeran <natkeeran@gmail.com>
2020-04-28 10:55:09 -0400

    add css rules to hide in colleciton markup for 3 content models

--- js/hide_in_collections.js
+++ js/hide_in_collections.js
@@ -2,5 +2,11 @@
   $(document).ready(function() {
     $("div.islandora-metadata-sidebar ~ div h2").hide();
     $("div.islandora-metadata-sidebar ~ div ul li").hide();
+    $("div.islandora-large-image-metadata div h2").hide();
+    $("div.islandora-large-image-metadata div ul li").hide();
+    $("div.islandora-basic-image-metadata div h2").hide();
+    $("div.islandora-basic-image-metadata div ul li").hide();
+    $("div.islandora-oralhistories-metadata div h2").hide();
+    $("div.islandora-oralhistories-metadata div ul li").hide();
  });
 })(jQuery);
